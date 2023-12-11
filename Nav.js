c3129b76425d15c3f6f909411149c2ea320c34f3
Nandanam-Vaishnavi <109271123+Nandanam-Vaishnavi@users.noreply.github.com>
2023-12-11 13:55:47 +0530

    Add files via upload

---
+++ Nav.js
@@ -0,0 +1,13 @@
+import React from "react"
+function nav() {
+    return(
+        <nav>
+            <img src="../react-icon-small.png" alt="React Logo" className="nav--icon"/>
+            <h3 className="nav--logo_text">React Facts</h3>
+            <h4 className="nav--title">React Course - Project 1</h4>
+        <h1>NAV</h1>
+        </nav>
+    )
+}
+
+export default nav;
